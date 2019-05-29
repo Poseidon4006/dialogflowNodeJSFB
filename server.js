41f278a5526b6962b028b8dd56ab78178f63eab0
Poseidon <andersonokram@gmail.com>
2019-05-29 14:34:23 +0530

    consoling action

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/', function(request, response) {
 app.post('/webhook/', function(request, response) {
   const dialogflowRequest = request.body;
   // An intent's action serves as a mapping mechanism between your intent and a function living in your app.
-  // const action = dialogflowRequest.queryResult.action;
-  
+   const action = dialogflowRequest.queryResult.action;
+  console.log(action);
   // these 3 variables could come from your intent's parameters !
   // const tabName = 'schoolDemo1';
   // const startCell = 'A2';
